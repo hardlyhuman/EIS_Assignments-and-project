e9cb9c9d1b034cb9e1761e523990c9bc01a31276
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-04-29 11:56:28 +0530

    First Stable commit of MajorProject

---
+++ software/water_management/developers/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def developers(request):
+	return render(request,'developers.html')
---
+++ software/water_management/water_management/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url,include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+	url(r'^$', include('index.urls')),
+	url(r'^login/',include('login.urls')),
+	url(r'^about/',include('about.urls')),
+	url(r'^developers/',include('developers.urls')),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
